f18e63fbb952fc87089a2067076fe789e7a5fd67
San4eeez <AlexandroHell@yandex.ru>
2024-01-02 22:50:48 +0300

    da

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,32 +1,33 @@
 const  db = require('../db')
 class UserController{
     async createUser(req,res){
-        const {name,surname} = req.body
-        const newPerson = await db.query('INSERT INTO person (name,surname) values ($1,$2) RETURNING *',[name,surname])
+        const {name,height,weight,base_experience,types,abilities,hp,attack,defense,special_attack,special_defense,speed} = req.body
+        const newPerson = await db.query('INSERT INTO pokemon (name,height,weight,base_experience,types,abilities,hp,attack,defense,special_attack,special_defense,speed) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) RETURNING *',[name,height,weight,base_experience,JSON.stringify(types),JSON.stringify(abilities),hp,attack,defense,special_attack,special_defense,speed])
         res.json(newPerson.rows[0])
     }
 
     async getUsers(req,res){
 
-        const users = await db.query('SELECT * FROM PERSON')
+        const users = await db.query('SELECT * FROM pokemon')
         res.json(users.rows)
     }
 
     async GetOneUser(req,res){
         const id = req.params.id
-        const users = await db.query('SELECT * FROM PERSON where id = $1',[id])
+        const users = await db.query('SELECT * FROM pokemon where id = $1',[id])
         res.json(users.rows)
     }
 
-    async updateUser(req,res){
-        const {id,name,surname} = req.body
-        const user = await db.query('update person set name = $1, surname = $2 where id = $3 returning *',[name,surname,id])
-        res.json(user.rows[0])
+    async updateUser(req, res) {
+        const { name, height, weight, base_experience, types, abilities, hp, attack, defense, special_attack, special_defense, speed, id } = req.body;
+        const user = await db.query('UPDATE pokemon SET name = $1, height = $2, weight = $3, base_experience = $4, types = $5, abilities = $6, hp = $7, attack = $8, defense = $9, special_attack = $10, special_defense = $11, speed = $12 WHERE id = $13 RETURNING *', [name, height, weight, base_experience, JSON.stringify(types), JSON.stringify(abilities), hp, attack, defense, special_attack, special_defense, speed, id]);
+        res.json(user.rows[0]);
     }
+
 
     async deleteUser(req,res){
         const id = req.params.id
-        const users = await db.query('delete FROM PERSON where id = $1',[id])
+        const users = await db.query('delete FROM pokemon where id = $1',[id])
         res.json(users.rows)
     }
 
--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     password: "admin",
     host: "localhost",
     port: 5432,
-    database: "js"
+    database: "pokemonDB"
 })
 
 module.exports = pool
