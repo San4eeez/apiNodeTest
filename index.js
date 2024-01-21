d0bfdb624ccfbc34512ef47f6c13328e2a3e1ba9
San4eeez <AlexandroHell@yandex.ru>
2024-01-21 15:45:40 +0300

    Картинки в апи

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const userRouter = require('./routes/user.routes')
+const helmet = require('helmet');
+const userRouter = require('./routes/user.routes');
 
+const PORT = process.env.PORT || 8080;
 
+const app = express();
 
-const PORT = process.env.PORT || 8080
-
-
-const app = express()
 app.use(cors());
-app.use(express.json())
-app.use('/api',userRouter)
+app.use(helmet());
+app.use(express.json());
 
-app.listen(PORT,() => console.log(`server started on post ${PORT}`))
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+app.use('/images', express.static('images'));
+app.use('/api', userRouter);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
